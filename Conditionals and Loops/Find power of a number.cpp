0dd90340f0b63f6c117f754a93f8d35752b5b8ef
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-06-08 00:49:56 +0530

    Add files via upload

---
+++ Conditionals and Loops/Find power of a number.cpp
@@ -0,0 +1,9 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+int main() {
+    int x,i;
+    cin>>x>>i;
+    int ans=pow(x,i);
+    cout<<ans;
+}
---
+++ Conditionals and Loops/Start Pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+    int i = 1;
+        while(i<=n){
+        int k =1;
+        while(k<=n - i){
+        cout<<" ";
+        k=k+1;
+        }
+        int j=1;  
+        while (j<=2*i-1){
+        cout<<"*";
+        j=j+1;
+        }
+		cout << endl;
+		i = i + 1;
+    }
+}
