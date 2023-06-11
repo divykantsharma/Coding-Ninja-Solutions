52aaee81f70a89270f8cfa158e50d5cdfa2d2242
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-06-11 21:40:20 +0530

    Delete Character Arrays and 2D Arrays directory

--- Character Arrays and 2D Arrays/Replace Character.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
-void replaceCharacter(char input[], char c1, char c2){
-    // Write your code here
-    for (int i = 0; input[i] != '\0'; i++){
-        if (input[i] == c1) input[i] = c2;
-    }
-}
